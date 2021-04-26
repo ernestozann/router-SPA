528f82ab83c823829bd488b917da064fa3823a93
Ernesto Martinez <ernestomm99@hotmail.com>
2021-04-26 13:10:38 -0600

    actualizando nuestro index.html

--- router.js
+++ router.js
@@ -1,10 +1,12 @@
 class Router {
+
     constructor(routes) {
         this.routes = routes;
         this._loadInitialRoute();
     }
 
-    loadRoute(...urlSegs){
+    loadRoute(...urlSegs) {
+
         const matchedRoute = this._matchUrlToRoute(urlSegs);
 
         const url = `/${urlSegs.join('/')}`;
@@ -12,24 +14,32 @@ class Router {
 
         const routerOutElm = document.querySelectorAll('[data-router]')[0];
         routerOutElm.innerHTML = matchedRoute.template;
+
     }
 
-    _matchUrlToRoute(urlSegs){
+    _matchUrlToRoute(urlSegs) {
         const matchedRoute = this.routes.find(route => {
+
             const routePathSegs = route.path.split('/').slice(1)
-            if (routePathSegs.length !== urlSegs.length) {
+
+            if (routePathSegs.length !== urlSegs.length){
                 return false;
             }
+
             return routePathSegs
                 .every((routePathSeg, i) => routePathSegs === urlSegs[i]);
         });
+
         return matchedRoute;
     }
 
+
     _loadInitialRoute() {
+        
         const pathNameSplit = window.location.pathname.split('/');
-        const pathSegs = pathNameSplit.length > 1 ? pathNameSplit(1): '';
+        const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1): '';
 
         this.loadRoute(...pathSegs);
     }
+    
 }
